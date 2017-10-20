cfe40a0f1c49cd90926359c1498253c1b7c9b975
福原　直也 <fukuharan@B1471-151201.local>
2017-10-20 13:35:09 +0900

    start main project

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function() {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが500に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
+$(function() {
+  $('a[href^="#"]').click(function() {
+    var speed = 500;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('html, body').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+});
