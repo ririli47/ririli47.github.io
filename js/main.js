cb6b414adc3be50ec808ea89656c65ca08f6d423
Naoya Fukuhara <suguha47@gmail.com>
2017-11-08 01:03:59 +0900

    add fadein title

--- js/main.js
+++ js/main.js
@@ -53,4 +53,18 @@ $(function() {
         $('#works').stop().animate({ backgroundColor: "#ffffff" }, 400);
       }
   });
-})
+});
+
+
+
+$(function() {
+  var $allMsg = $('#title');
+  var $wordList = $('#title').html().split("");
+  $('#title').html("");
+  $.each($wordList, function(idx, elem) {
+    var newEL = $("<span/>").text(elem).css({ opacity: 0 });
+    newEL.appendTo($allMsg);
+    newEL.delay(idx * 70);
+    newEL.animate({ opacity: 1 }, 1100);
+  });
+});
