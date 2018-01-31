fa3c468ede54baef6a8dffdc87452e7ab66c4c3a
Naoya Fukuhara <suguha47@gmail.com>
2018-01-31 23:38:05 +0900

    Swiperを設定してTaskerの画像をかえた
    Swiperを設定してTaskerの画像をかえました

--- js/main.js
+++ js/main.js
@@ -50,9 +50,9 @@ $(function() {
 $(function() {
   var mySwiper = new Swiper ('.swiper-container', {
 	loop: true,
-	slidesPerView: 2,
-  	spaceBetween: 10,
-  	centeredSlides : true,
+  slidesPerView: 2,
+	spaceBetween: 10,
+	centeredSlides : true,
 	pagination: '.swiper-pagination',
 	nextButton: '.swiper-button-next',
 	prevButton: '.swiper-button-prev'
